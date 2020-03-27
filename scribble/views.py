ac5df8d45772eae1065848c7bf782f2aacbf8c36
Darryl Hood <darryl.r.hood@gmail.com>
2020-03-27 01:56:41 -0400

    created list functions

--- scribble/views.py
+++ scribble/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post, Comment
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'scribble/post_list.html', {'posts': posts})
+
+def comment_list(request):
+    comments = Comment.objects.all()
+    return render(request, 'scribble/comment_list.html', {'comments': comments})
