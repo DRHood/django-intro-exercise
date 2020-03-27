f1ed5789b05a312778620aa0c458ee267e96674e
Darryl Hood <darryl.r.hood@gmail.com>
2020-03-27 01:26:38 -0400

    corrected models

--- scribble/models.py
+++ scribble/models.py
@@ -6,7 +6,7 @@ class Post(models.Model):
     body = models.CharField(max_length=300)
 
     def __str__(self):
-        return self.name
+        return self.author
 
 class Comment(models.Model):
     author = models.CharField(max_length=100)
@@ -14,4 +14,4 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
 
     def __str__(self):
-        return self.name
+        return self.author
