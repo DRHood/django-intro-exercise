c536ae18fe781b28444aaf5b01dd262faeb80604
Darryl Hood <darryl.r.hood@gmail.com>
2020-03-27 10:11:34 -0400

    trying to keep up in class, after missing most of yesterday's lesson

--- scribble/urls.py
+++ scribble/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='posts_list'),
-    path('', views.comment_list, name='comments_list'),
+    path()
+
+    path('comments/new', views.comment_create, name='comment_create'),
 ]
--- scribble/views.py
+++ scribble/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import CommentForm, PostForm
 from .models import Post, Comment
 
 def post_list(request):
     posts = Post.objects.all()
     return render(request, 'scribble/post_list.html', {'posts': posts})
 
+def post_details(request):
+    post.objects.get(pk=pk)
+    return render(request, 'scribble/post_detail.html', )
+
 def comment_list(request):
     comments = Comment.objects.all()
     return render(request, 'scribble/comment_list.html', {'comments': comments})
+
+def post_delete(request, pk):
+    post.object..get(pk=pk).delete()
+    return.redirect
